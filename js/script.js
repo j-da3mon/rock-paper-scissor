bfcf6b33b8da823cb167da1dda80a5aae2fb58d8
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-08 20:49:02 -0400

    add choice functions and set score to 0

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 console.log("Hello, World!")
 
+// Get computer choice
+
+const computerChoice = function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    if (randomChoice === 0) {
+        return "Rock"
+    } 
+        else if (randomChoice === 1) {
+            return "Paper"
+        }
+        else {
+            return "Scissor"
+        }
+}
+
+//get human choice
+
+const humanChoice = function getHumanChoice() {
+    let userChoice = prompt("Choose Rock, Paper, or Scissor")
+    return userChoice
+}
+
+console.log(computerChoice())
+console.log(humanChoice())
+
+// set scores to 0
+
+let computerScore = 0
+let humanScore = 0
+
+console.log(computerScore)
+console.log(humanScore)
