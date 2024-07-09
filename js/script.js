6bfd1ae342acddd73055383ace6ac8712c84053f
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-09 19:14:57 -0400

    add playRound to rock, paper, scissor game

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log("Hello, World!")
 
 // Get computer choice
 
-const computerChoice = function getComputerChoice() {
+function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
     if (randomChoice === 0) {
         return "Rock"
@@ -15,20 +15,56 @@ const computerChoice = function getComputerChoice() {
         }
 }
 
+//show computer's choice for testing
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice)
+
 //get human choice
 
-const humanChoice = function getHumanChoice() {
+function getHumanChoice() {
     let userChoice = prompt("Choose Rock, Paper, or Scissor")
     return userChoice
 }
 
-console.log(computerChoice())
-console.log(humanChoice())
+const humanChoice = getHumanChoice();
+console.log(humanChoice)
 
 // set scores to 0
 
 let computerScore = 0
 let humanScore = 0
+
+// play a round
+
+function playRound(humanChoice, computerChoice) {
+    lowerhuman = humanChoice.toLowerCase()
+    lowercomputer = computerChoice.toLowerCase()
+
+    if (lowerhuman == lowercomputer) {
+        console.log("Tie. Please choose again!")}
+        else if (lowerhuman == "rock" && lowercomputer == "scissor") {
+            console.log("Human wins a point!")
+            humanScore ++
+        }
+        else if (lowerhuman == "paper" && lowercomputer == "rock") {
+            console.log("Human wins a point!")
+            humanScore ++
+        }
+        else if (lowerhuman == "scissor" && lowercomputer == "paper") {
+            console.log("Human wins a point!")
+            humanScore ++
+        }
+        else {
+            console.log("Computer wins a point!")
+            computerScore ++
+        }
+
+}
+
+playRound(humanChoice, computerChoice)
+
+// play a game of five rounds
 
 console.log(computerScore)
 console.log(humanScore)
